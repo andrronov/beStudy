c0471a2c1ba2101f1133119a74d5ec0445cb17ba
Андрей Андронов <andrew270504cska@gmail.com>
2024-03-04 17:28:19 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  hello
+  <router-view />
 </template>
 
 <script setup>
---
+++ src/pages/mainPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-screen h-screen bg-gray-50 text-xl">
+   <p>aaa</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
