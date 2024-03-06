8b04d4cb1725769faf1d954a8849b64aac2778bc
Андрей Андронов <andrew270504cska@gmail.com>
2024-03-06 20:37:02 +0300

    start

---
+++ src/components/modalComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fixed z-20 w-screen h-screen bg-gray-800">
+   <div class="w-full h-full flex items-center justify-center">
+      <slot></slot>
+   </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
--- src/pages/mainPage.vue
+++ src/pages/mainPage.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="w-screen h-screen bg-gray-50 text-xl">
-   <p>aaa</p>
+  <!-- <mainPage /> -->
+
+  <div class="w-screen h-screen bg-gray-100">
+   <div class="max-w-7xl h-full mx-auto flex flex-col items-center justify-evenly">
+    <h1 class="text-3xl sm:text-4xl font-semibold">Be Study</h1>
+    <div class="flex flex-col items-center gap-4 text-xl sm:text-2xl">
+      <p class="mb-8">Welcome, Andrew!</p>
+      <button class="px-3 py-2 bg-red-600 text-white rounded-xl hover:bg-red-700">Создать карточку</button>
+      <button class="px-3 py-2 bg-red-600 text-white rounded-xl hover:bg-red-700">Редактировать коллекцию</button>
+      <button class="px-3 py-2 bg-red-600 text-white rounded-xl hover:bg-red-700">Пройти тест</button>
+    </div>
+   </div>
   </div>
 </template>
 
 <script setup>
-
+import mainPage from '../components/modalComponent.vue'
 </script>
 
 <style>
